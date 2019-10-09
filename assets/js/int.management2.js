537c211b25b08a2aecfdb880cc9d18c384cacd26
tokazato <tokazato1992>
2019-10-09 20:43:08 +0400

    day 3
    day 3

--- assets/js/int.management2.js
+++ assets/js/int.management2.js
@@ -3,4 +3,8 @@ var swiper = new Swiper('.swiper-container', {
       nextEl: '.management2-slider-next-button',
       prevEl: '.management2-slider-prev-button',
     },
+    scrollbar: {
+      el: '.management2-swiper-scroll',
+      hide: false,
+    },
   });
