dbf901149c10fb3839c0661be324c747fd697282
tokazato <tokazato1992>
2019-10-14 20:34:17 +0400

    day 5
    day 5

--- assets/js/int.inservice.js
+++ assets/js/int.inservice.js
@@ -4,11 +4,15 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.inservice-swiper-arrow-buttons-prev',
     },
     spaceBetween: 100,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
   });
 
   
   swiper.on('slideChange', function () {
-    let goDown = (110 / 5 * this.realIndex);
+    let goDown = (90 / 5 * this.realIndex);
     console.log(goDown);
     document.getElementById('circle').style.transform = `translateY(-50%) rotate(${goDown}deg)`;
   });
